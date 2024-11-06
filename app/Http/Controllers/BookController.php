aaa3e0b1c97254280619a9e6113899757e8727a3
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-11-06 14:26:09 +0100

    Displaying

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request )
+    {
+        $title = $request->input('title');
+        $books = Book::when($title, function($query, $title){
+            return $query->title($title);
+
+        })->get();
+
+        return view('books.index', ['books' => $books]);
+
+        // return view('books.index', compact('books')); signfifie que je veux retourner une vue et e vais passer la variable $books a ma vue
+
+        // $books = Book::when($title, fn($query, $title)=>
+        // $query->title($title)
+        // )->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
